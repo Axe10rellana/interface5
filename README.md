38f2082722625cb98e3e85ec5c5fe17480756aea
Axe10rellana <axelorellana550@gmail.com>
2021-10-18 19:30:42 -0300

    add interface proyect

---
+++ README.md
@@ -0,0 +1,5 @@
+# Interfaz De Venta De Cafes
+
+Repositorio de interfaz hecho en React.js
+
+- [Url de la interfaz](https://Axe10rellana.github.io/interface5/interface5)
---
+++ interface5/static/js/main.6689abdf.chunk.js
@@ -0,0 +1,496 @@
+(this["webpackJsonp06-practica"] = this["webpackJsonp06-practica"] || []).push([
+  [0],
+  {
+    12: function (e, A, i) {
+      "use strict";
+      i.r(A);
+      var t = i(1),
+        a = i.n(t),
+        c = i(3),
+        r = i.n(c),
+        s = (i(8), i(0)),
+        d = function () {
+          return Object(s.jsxs)("div", {
+            className: "header",
+            children: [
+              Object(s.jsxs)("div", {
+                className: "menu-left",
+                children: [
+                  Object(s.jsx)("a", { href: "#coffee", children: "Coffee" }),
+                  Object(s.jsx)("a", {
+                    href: "#merchandise",
+                    children: "Merchandise",
+                  }),
+                  Object(s.jsx)("a", { href: "#gifts", children: "Gifts" }),
+                  Object(s.jsx)("a", {
+                    href: "#subscriptions",
+                    children: "Suscriptions",
+                  }),
+                ],
+              }),
+              Object(s.jsxs)("div", {
+                className: "menu-right",
+                children: [
+                  Object(s.jsxs)("a", {
+                    href: "#guides",
+                    children: [
+                      "Brewing Guides ",
+                      Object(s.jsx)("i", { className: "fas fa-chevron-down" }),
+                    ],
+                  }),
+                  Object(s.jsxs)("a", {
+                    href: "#cafes",
+                    children: [
+                      "Cafes ",
+                      Object(s.jsx)("i", { className: "fas fa-chevron-down" }),
+                    ],
+                  }),
+                  Object(s.jsx)("a", { href: "#story", children: "Our Story" }),
+                  Object(s.jsx)("a", { href: "#blog", children: "Blog" }),
+                  Object(s.jsx)("a", {
+                    className: "signin",
+                    href: "#signin",
+                    children: "Sign In",
+                  }),
+                  Object(s.jsx)("a", {
+                    className: "cart",
+                    href: "#cart",
+                    children: Object(s.jsx)("i", {
+                      className: "fas fa-shopping-cart",
+                    }),
+                  }),
+                ],
+              }),
+            ],
+          });
+        },
+        n = function (e) {
+          var A = e.title,
+            i = e.text,
+            t = e.price,
+            a = e.image;
+          return Object(s.jsx)("section", {
+            children: Object(s.jsxs)("div", {
+              className: "card text-left animate__animated animate__fadeInUp",
+              children: [
+                Object(s.jsx)("div", {
+                  className: "overflow",
+                  children: Object(s.jsx)("img", {
+                    src: a,
+                    alt: A,
+                    className: "card__img card__img__top",
+                  }),
+                }),
+                Object(s.jsxs)("div", {
+                  className: "card-body",
+                  children: [
+                    Object(s.jsxs)("div", {
+                      className: "card__body__flex",
+                      children: [
+                        Object(s.jsx)("h4", {
+                          className: "card-title subtitle",
+                          children: A,
+                        }),
+                        Object(s.jsx)("p", { children: t }),
+                      ],
+                    }),
+                    Object(s.jsx)("p", {
+                      className: "card-text text-secondary",
+                      children: i,
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          });
+        },
+        f = [
+          {
+            id: 1,
+            title: "Bella Donovan",
+            price: "$19.00",
+            text: "Heavy, Conforting, deeply fruited",
+            image:
+              i.p + "interface5/interface5/static/media/image1.63d86bc9.jpg",
+          },
+          {
+            id: 2,
+            title: "Three Africans",
+            price: "$19.00",
+            text: "Frutty, radiant, creamy",
+            image:
+              i.p + "interface5/interface5/static/media/image2.0c855af6.jpg",
+          },
+          {
+            id: 3,
+            title: "Giant steps",
+            price: "$19.00",
+            text: "Viscout, fudgy, substantial",
+            image:
+              i.p + "interface5/interface5/static/media/image3.bda06d38.jpg",
+          },
+          {
+            id: 4,
+            title: "Decaf Noir",
+            price: "$20.00",
+            text: "Nutty, chocolaty, dense",
+            image:
+              i.p + "interface5/interface5/static/media/image4.aeab132f.jpg",
+          },
+          {
+            id: 5,
+            title: "Hayes Valley Espresso",
+            price: "$19.00",
+            text: "Cocoa, orange zest, smoky finish",
+            image:
+              i.p + "interface5/interface5/static/media/image5.3bc6be82.jpg",
+          },
+          {
+            id: 6,
+            title: "17ft Ceiling",
+            price: "$19.00",
+            text: "Effulgent caramelly, enveloping, nutty",
+            image:
+              i.p + "interface5/interface5/static/media/image6.8481c90b.jpg",
+          },
+          {
+            id: 7,
+            title: "Roman Espresso",
+            price: "$19.00",
+            text: "Jammy, malted, medium-bodied",
+            image:
+              i.p + "interface5/interface5/static/media/image7.37f23a4c.jpg",
+          },
+          {
+            id: 8,
+            title: "Retrofit Espresso",
+            price: "$19.00",
+            text: "Sweet, delcate, balanced",
+            image:
+              i.p + "interface5/interface5/static/media/image8.aeaaef00.jpg",
+          },
+          {
+            id: 9,
+            title: "House of Good",
+            price: "$20.00",
+            text: "Sweet, delicate, balanced",
+            image:
+              i.p + "interface5/interface5/static/media/image9.cf580077.jpg",
+          },
+          {
+            id: 10,
+            title: "Drip Medley Subscription",
+            price: "$19.00",
+            image:
+              i.p + "interface5/interface5/static/media/image10.9f0fc4c7.jpg",
+          },
+          {
+            id: 11,
+            title: "Espresso Medley Subscription",
+            price: "$19.00",
+            image:
+              i.p + "interface5/interface5/static/media/image11.ca8885fb.jpg",
+          },
+          {
+            id: 12,
+            title: "French Press Medley Subscription",
+            price: "$19.00",
+            image:
+              i.p + "interface5/interface5/static/media/image12.56dfdc12.jpg",
+          },
+          {
+            id: 13,
+            title: "Moka Pot Medley Subscription",
+            price: "$19.00",
+            image:
+              i.p + "interface5/interface5/static/media/image13.d073d92b.jpg",
+          },
+          {
+            id: 14,
+            title: "New Orleans Iced Kit",
+            price: "$18.00",
+            text: "Rich, sweet, syrupy",
+            image:
+              i.p + "interface5/interface5/static/media/image14.757054a3.jpg",
+          },
+          {
+            id: 15,
+            title: "Brazil FAF Santa Clara icatu Natural",
+            price: "$24.00",
+            text: "Papaya, rose, brown sugar, sparkiling acidity",
+            image:
+              i.p + "interface5/interface5/static/media/image15.e7092d2d.jpg",
+          },
+          {
+            id: 16,
+            title: "Brazil FAF Santa Clara Obata Natural",
+            price: "$12.00",
+            text: "Cramberry jam, wine, rhubarb",
+            image:
+              i.p + "interface5/interface5/static/media/image16.91399d10.png",
+          },
+          {
+            id: 17,
+            title: "Burundi Twese Twoterimbere",
+            price: "$22.00",
+            text: "Key lime pie, apricot nectar, brown sugar",
+            image:
+              i.p + "interface5/interface5/static/media/image17.1bb374f1.jpg",
+          },
+          {
+            id: 18,
+            title: "Cascara Tea",
+            price: "$7.50",
+            text: "Floral, honey-sweet, jasmine, camells blossoms",
+            image:
+              i.p + "interface5/interface5/static/media/image18.08d96016.jpg",
+          },
+          {
+            id: 19,
+            title: "El Salvador Tres Limites Natural",
+            price: "$12.00",
+            text: "Raspberry soda, black currant, syrupy, complex, wine-lime",
+            image:
+              i.p + "interface5/interface5/static/media/image19.410c2591.jpg",
+          },
+          {
+            id: 20,
+            title: "Ethiopia Yirgacheffe Gelena Abaya Natural",
+            price: "$24.00",
+            text: "Violet, cocoa, stone fruit",
+            image:
+              i.p + "interface5/interface5/static/media/image20.9f56f494.jpg",
+          },
+          {
+            id: 21,
+            title: "Mexico La Ca\xf1ada",
+            price: "$15.00",
+            text: "Blackberry, dark cherry, cinnamon, orange",
+            image:
+              i.p + "interface5/interface5/static/media/image21.be5c53bd.jpg",
+          },
+          {
+            id: 22,
+            title: "Mexico Oaxaca Mixteca",
+            price: "$19.00",
+            text: "Peanut butter, silky, red fruit, bright",
+            image:
+              i.p + "interface5/interface5/static/media/image22.3f8c7ba9.jpg",
+          },
+          {
+            id: 23,
+            title: "Rwanda Kirezi",
+            price: "$22.00",
+            text: "Black tea, citrus, gried pluff, floral",
+            image:
+              i.p + "interface5/interface5/static/media/image23.12fed452.jpg",
+          },
+          {
+            id: 24,
+            title: "Sumatra Lintong Lake Tawar",
+            price: "$22.00",
+            text: "Papaya, cola, chocolate ganache",
+            image:
+              i.p + "interface5/interface5/static/media/image24.8a171e91.png",
+          },
+        ],
+        l = function () {
+          return Object(s.jsx)("div", {
+            className: "container",
+            children: Object(s.jsx)("div", {
+              className: "row",
+              children: f.map(function (e, A) {
+                return Object(s.jsx)(
+                  "div",
+                  {
+                    className: "col-lg-3 col-sm-6",
+                    children: Object(s.jsx)(n, {
+                      title: e.title,
+                      image: e.image,
+                      text: e.text,
+                      price: e.price,
+                    }),
+                  },
+                  A
+                );
+              }),
+            }),
+          });
+        },
+        o = function (e) {
+          var A = e.image,
+            i = e.title;
+          return Object(s.jsx)("section", {
+            children: Object(s.jsx)("div", {
+              className: "card text-left",
+              children: Object(s.jsx)("div", {
+                className: "overflow",
+                children: Object(s.jsx)("img", {
+                  src: A,
+                  alt: i,
+                  className: "card__img",
+                }),
+              }),
+            }),
+          });
+        },
+        g = [
+          {
+            id: 1,
+            title: "Moka Pot Medley",
+            image: i.p + "interface5/interface5/static/media/icon1.jpg",
+          },
+          {
+            id: 2,
+            title: "French Press Medley",
+            image: i.p + "interface5/interface5/static/media/icon2.png",
+          },
+          {
+            id: 3,
+            title: "Espresso Medley",
+            image: i.p + "interface5/interface5/static/media/icon3.png",
+          },
+        ],
+        U = function () {
+          return Object(s.jsx)("div", {
+            className: "container",
+            children: Object(s.jsx)("div", {
+              className: "row",
+              children: g.map(function (e, A) {
+                return Object(s.jsx)(
+                  "div",
+                  {
+                    className: "col-lg-4 col-sm-6",
+                    children: Object(s.jsx)(o, {
+                      image: e.image,
+                      title: e.title,
+                    }),
+                  },
+                  A
+                );
+              }),
+            }),
+          });
+        },
+        m = function () {
+          return Object(s.jsxs)("div", {
+            className: "main",
+            children: [
+              Object(s.jsx)("h2", { children: "Coffee" }),
+              Object(s.jsxs)("div", {
+                className: "icons__container",
+                children: [
+                  Object(s.jsx)("h3", {
+                    children: "How do you brew your coffee?",
+                  }),
+                  Object(s.jsx)("div", {
+                    className: "icons",
+                    children: Object(s.jsx)(U, {}),
+                  }),
+                ],
+              }),
+              Object(s.jsx)("div", {
+                className: "cards",
+                children: Object(s.jsx)(l, {}),
+              }),
+            ],
+          });
+        },
+        j = function () {
+          return Object(s.jsxs)("div", {
+            className: "footer",
+            children: [
+              Object(s.jsxs)("div", {
+                className: "footer__top",
+                children: [
+                  Object(s.jsx)("a", { href: "#contact", children: "Contact" }),
+                  Object(s.jsx)("a", { href: "#careers", children: "Careers" }),
+                  Object(s.jsx)("a", {
+                    href: "#wholesale",
+                    children: "Wholesale",
+                  }),
+                  Object(s.jsx)("a", {
+                    href: "#specialorders",
+                    children: "Special Orders",
+                  }),
+                  Object(s.jsx)("a", { href: "#press", children: "Press" }),
+                  Object(s.jsx)("a", {
+                    href: "#experiences",
+                    children: "Experiences",
+                  }),
+                  Object(s.jsx)("a", { href: "#events", children: "Events" }),
+                  Object(s.jsx)("a", {
+                    href: "#modernartdesserts",
+                    children: "Modern Art Desserts",
+                  }),
+                ],
+              }),
+              Object(s.jsxs)("div", {
+                className: "footer__bottom",
+                children: [
+                  Object(s.jsx)("div", {
+                    className: "footer__bottom__1",
+                    children: Object(s.jsxs)("form", {
+                      children: [
+                        Object(s.jsx)("input", {
+                          type: "email",
+                          name: "email",
+                          placeholder: "  Enter email for news and updates",
+                          autoComplete: "off",
+                          required: !0,
+                        }),
+                        Object(s.jsx)("input", {
+                          type: "submit",
+                          value: "Sign up",
+                        }),
+                      ],
+                    }),
+                  }),
+                  Object(s.jsxs)("div", {
+                    className: "footer__bottom__2",
+                    children: [
+                      Object(s.jsx)("a", {
+                        href: "#shippingandreturn",
+                        children: "Shipping And Return",
+                      }),
+                      Object(s.jsx)("a", {
+                        href: "#termsofuse",
+                        children: "Terms of Use",
+                      }),
+                      Object(s.jsx)("a", {
+                        href: "#privacy",
+                        children: "Privacy",
+                      }),
+                      Object(s.jsxs)("p", {
+                        children: [
+                          Object(s.jsx)("i", { className: "far fa-copyright" }),
+                          " 2013 Blue Bottle Coffe Inc, All Rights Reserved",
+                        ],
+                      }),
+                    ],
+                  }),
+                ],
+              }),
+            ],
+          });
+        },
+        F = function () {
+          return Object(s.jsxs)("div", {
+            children: [
+              Object(s.jsx)(d, {}),
+              Object(s.jsx)(m, {}),
+              Object(s.jsx)(j, {}),
+            ],
+          });
+        };
+      i(10), i(11);
+      r.a.render(
+        Object(s.jsx)(a.a.StrictMode, { children: Object(s.jsx)(F, {}) }),
+        document.getElementById("root")
+      );
+    },
+    8: function (e, A, i) {},
+  },
+  [[12, 1, 2]],
+]);
+//# sourceMappingURL=main.6689abdf.chunk.js.map
